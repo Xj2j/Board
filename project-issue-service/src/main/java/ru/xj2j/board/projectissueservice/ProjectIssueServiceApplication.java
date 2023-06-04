82b6544f437d4adbe990552cb8a853586a52bcd3
Xj2j <slavgoth.pr@gmail.com>
2023-06-04 22:59:33 +0300

    feat: add project-issue-service

---
+++ project-issue-service/src/main/java/ru/xj2j/board/projectissueservice/ProjectIssueServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.xj2j.board.projectissueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIssueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIssueServiceApplication.class, args);
+	}
+
+}
---
+++ project-issue-service/src/test/java/ru/xj2j/board/projectissueservice/ProjectIssueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.xj2j.board.projectissueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIssueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
