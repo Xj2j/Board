ec5729d5439b5eac0af49a51d227d6d0654abf7a
Xj2j <slavgoth.pr@gmail.com>
2023-06-04 23:00:17 +0300

    feat: add models

---
+++ board-entity/src/main/java/ru/xj2j/board/entity/JsonConverter.java
@@ -0,0 +1,32 @@
+package ru.xj2j.board.entity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.persistence.AttributeConverter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Could not convert map to JSON string", e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String dbData) {
+        try {
+            return objectMapper.readValue(dbData, HashMap.class);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not convert JSON string to map", e);
+        }
+    }
+}
---
+++ board-entity/src/main/java/ru/xj2j/board/entity/WorkspaceTheme.java
@@ -0,0 +1,65 @@
+package ru.xj2j.board.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@Entity
+@Table(name = "workspace_themes")
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true) // наследуем equals() и hashCode() от BaseModel
+@ToString(callSuper = true) // наследуем toString() от BaseModel
+public class WorkspaceTheme extends BaseModel {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id", nullable = false)
+    @JsonIgnoreProperties("themes")
+    private Workspace workspace;
+
+    @Column(name = "name", length = 300, nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "actor_id", nullable = false)
+    @JsonIgnoreProperties("themes")
+    private User actor;
+
+    @Column(name = "colors", columnDefinition = "jsonb", nullable = false)
+    @Convert(converter = JsonConverter.class)
+    private Map<String, String> colors = new HashMap<>();
+
+    @JsonCreator
+    public WorkspaceTheme(@JsonProperty("workspace") Workspace workspace,
+                          @JsonProperty("name") String name,
+                          @JsonProperty("actor") User actor,
+                          @JsonProperty("colors") Map<String, String> colors) {
+        this.workspace = workspace;
+        this.name = name;
+        this.actor = actor;
+        this.colors = colors;
+    }
+
+    /**@PrePersist
+    void createdAt() {
+        this.createdAt = LocalDate.now();
+    }
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;*/
+}
